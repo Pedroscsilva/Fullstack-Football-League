16fe6dbecffdca7fff7f3ea7007fcf90e3ea270d
Pedroscsilva <pedroscsilva2001@gmail.com>
2023-03-02 14:12:52 -0300

    feat: add migrations and models for users

--- app/backend/src/database/migrations/20230302170712-create_users.js
+++ app/backend/src/database/migrations/20230302170712-create_users.js
@@ -2,18 +2,21 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('teams', { 
+    await queryInterface.createTable('users', { 
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      team_name: Sequelize.STRING,
+      username: Sequelize.STRING,
+      role: Sequelize.STRING,
+      email: Sequelize.STRING,
+      password: Sequelize.STRING,
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('teams');
+    await queryInterface.dropTable('users');
   }
 };
